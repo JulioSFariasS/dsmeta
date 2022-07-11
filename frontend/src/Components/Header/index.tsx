e2eb3115cc6a97be43f48d892708c3368837e1f7
Julio Santos <juliosfariass@gmail.com>
2022-07-11 20:28:27 -0300

    Alteração no Header

--- frontend/src/Components/Header/index.tsx
+++ frontend/src/Components/Header/index.tsx
@@ -8,8 +8,7 @@ function Header() {
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
                 <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    Desenvolvido por <a href="https://www.instagram.com/juliosfariass/">@juliosfariass</a> junto ao <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
                 </p>
             </div>
         </header>
